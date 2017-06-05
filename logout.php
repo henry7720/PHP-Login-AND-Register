293ae03b8f9fb295797ddbcaf11740cc69157cf3
henry7720 <henry.trowbridge2@gmail.com>
2017-06-04 17:22:05 -1000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
 session_destroy();
-header("Location: index.php");
+header("Location: http://example.com/login.php");
+# Can also use:
+# echo "<script>window.location = 'http://example.com/login.php'</script>";
 ?>
