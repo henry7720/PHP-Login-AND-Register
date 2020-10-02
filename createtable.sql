b63f068bd309510de83be357ee49953a8ab7639a
her_finn <58078450+her-finn@users.noreply.github.com>
2020-10-02 20:38:12 +0200

    Updatet
    removed salt and extended password-hash

--- createtable.sql
+++ createtable.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(12) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `salt` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
